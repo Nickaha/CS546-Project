7dd82785d29817a9c74d0eb6395d266e71e66e25
Nick Guo <nguo1@stevens.edu>
2021-04-27 11:47:01 -0400

    app.js update

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
-
-const exphbs = require('express-handlebars');
 const session = require('express-session');
+const configRoutes = require('./routes');
+const static = express.static(__dirname + '/public');
+const exphbs = require('express-handlebars');
 
+app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(session({
+    name: 'AuthCookie',
+    secret: 'some secret string!',
+    resave: false,
+    saveUninitialized: true
+}));
+
+configRoutes(app);
 app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
