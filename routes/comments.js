0414629f68cc52e7c1cfec928308724e16704295
yakovkazinets <yakovkazinets@gmail.com>
2021-05-08 18:11:04 -0400

    comments.js is not needed nor is the commentRoute module in index.js

--- routes/comments.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const data = require('/data');
-const bidData = data.bids;
-const commentsData = data.comments;
-const listingData = data.listings;
