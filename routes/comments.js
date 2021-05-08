1da0a0033d54e4fdc5a7b2f71c2a3c4e948d9e36
yakovkazinets <yakovkazinets@gmail.com>
2021-05-08 18:05:48 -0400

    comments.js in routes file, but not functional yet

---
+++ routes/comments.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const data = require('/data');
+const bidData = data.bids;
+const commentsData = data.comments;
+const listingData = data.listings;
