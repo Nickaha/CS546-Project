207f13578e5fd34980327c955c9f64bbe852245e
Nicholas Szegheo <nicholas.szegheo@gmail.com>
2021-05-08 23:40:18 -0400

    Added comments routes, with POST/DELETE

---
+++ routes/comments.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const commentData = data.comments;
+const listingData = data.listings;
+
+// POST
+router.post( '/:id', async (req, res) => {
+    // Returns JSON for use with AJAX.
+    // The idea is that we modify the DOM with JQuery after the request, but
+    // on page reload the new post will also be fetched.
+    if (!req.session.user){
+        return res.status(401).json({message: "Unauthorized."});
+    }
+
+    const listid = req.params.id;
+    const comment = req.body.comment; // POST body MUST have attribute "comment"!
+    const username = req.session.user.userName;
+
+    if (listid === undefined || listid === ""){
+        return res.status(400).json({message: "Listing ID undefined."});
+    }
+
+    if (!username){
+        return res.status(500).json({message: "Username missing from session data."});
+    }
+
+    if (comment === undefined || comment.trim() === ""){
+        return res.status(400).json({message: "Comment must be a non-empty string of characters containing more than spaces."});
+    }
+    try {
+        const insertion = await commentData.createComment(username, comment, listid);
+        return res.status(200).json({message: "Comment posted successfully."});
+    } catch (error) {
+        return res.status(500).json({message: "Failed to post comment."});
+    }
+
+});
+
+// DELETE
+router.delete('/:id', async (req, res) =>{
+    // Return JSON for use with AJAX. Delete the element with JQuery on the page,
+    // and remove from the database so that on reload it is missing.
+    if (!req.session.user){
+        return res.status(401).json({message: "Unauthorized."});
+    }
+    const commentid = req.params.id;
+    if (commentid === undefined || commentid === ""){
+        return res.status(400).json({message: "Comment ID undefined."});
+    }
+    const comment = await commentData.getCommentById(commentid);
+    if (req.session.user.userName !== comment.name){
+        return res.status(403).json({message: "Forbidden: Cannot delete other user's comment."});
+    }
+
+    try {
+        const deletion = await commentData.deleteComment(commentid);
+        return res.status(200).json({message: "Comment successfully deleted."});
+    } catch (error) {
+        return res.status(500).json({message: "Failed to delete comment."});
+    }
+
+});
