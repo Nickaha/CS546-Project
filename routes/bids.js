ee62883b17d5fd8a274f4b187519d1139f1c1657
Nicholas Szegheo <nicholas.szegheo@gmail.com>
2021-05-08 21:24:53 -0400

    Added POST/DELETE routes for bids

--- routes/bids.js
+++ routes/bids.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const data = require('../data');
 const bidData = data.bids;
 const listingData = data.listings
-
+let { ObjectId } = require('mongodb');
 
 router.get( '/', async (req, res) =>{
     if (!req.session.user){
@@ -48,40 +48,103 @@ router.get( '/', async (req, res) =>{
 } );
 
 router.post('/:id', async (req, res) => {
-    //TODO - Check user authentication
-    
-    console.log("POST bids/:id");
-    const listid = req.params.id;
-    // Also needed -- request body data with bid price.
-    
-    
-    //Check that id is provided, attempt to add bid to the listing,
-    // Go to ??? page with message "Bid was successfully placed."
-    if (listid === undefined || listid === ""){
-        return //res.render -- Not sure which page to visit
+    // This route will not return HTML - it returns JSON and a status code.
+    // Will be called through AJAX on listing pages.
+    // Check user authentication and data integrity. 
+
+    if (!req.session.user){
+        return res.status(401).json({message: "Unauthorized."});
     }
-    let retval = await bidData.createBid();
-    // TODO - Return to some page after request finishes.
+    let listid = req.params.id;
+    const bid = req.body.bid; // POST body MUST contain attribute "bid"!
+    const username = req.session.user.userName;
+    const userId = req.session.user._id;
+
+    try {
+        if (!listid || listid === ""){
+            throw "Listing ID missing.";
+        }else if (!bid || typeof(bid) !== 'number'){
+            throw "Bid missing or of invalid type.";
+        }else if (!username){
+            throw "Username not found.";
+        }
+    } catch (error) {
+        return res.status(400).json({message: error});
+    }
+    //Convert listid to ObjectID.
+    try {
+        listid = ObjectId(listid);
+    } catch (error) {
+        return res.status(400).json({message: "Invalid Object ID."});
+    }
+    listid = ObjectId(listid);
+
+    // After getting all our data, authenticate user to post to the bid. User cannot post to their own listing.
+    
+    const userListings = await listingData.getallofuser(userId);
+    for (let i = 0; i < userListings.length; i++){
+        // Verify that it is not the user's own bid.
+        if (listid.equals(userListings[i]._id)){
+            return res.status(403).json({message: "Forbidden: Cannot bid on own listing."});
+        }
+    }
+    // Post the bid.
+    try {
+        const bidMade = await bidData.createBid(username, bid, listid);
+        return res.status(200).json({message: "Bid successful."});
+    } catch (error) {
+        return res.status(500).json({message: "Failed to post bid."});
+    }
 });
 
 router.delete('/:id', async (req, res) => {
-    //TODO - Check user authentication
-    
-    const bidId = req.params.id;
-    //TODO - get user bids
-    
-    //Check that id is provided, attempt to delete, go to bids with message:
-    //"Bid was succcessfully deleted."
-    if (bidId === undefined || bidId === ""){
-        return //res.render -- How do I pass the message to the '/' route?
+    // This route will not return HTML - it returns JSON and a status code.
+    // Will be called through AJAX on listing/bids pages.
+    // Check user authentication and data integrity.
+    if (!req.session.user){
+        return res.status(401).json({message: "Unauthorized."});
+    }
+    if (!req.session.user._id){
+        return res.status(500).json({message: "User ID missing from session."});
     }
 
-    /*TODO - Only delete the bid if, after looping through user bids for the currently
+    let bidId = req.params.id;
+    const userBids = await bidData.getUserBids(req.session.user._id);
+
+    //Check that id is provided
+    if (bidId === undefined || bidId === ""){
+        return res.status(400).json({message: "Bid id missing."});
+    }
+    
+    //Convert bidId to ObjectID.
+    try {
+        bidId = ObjectId(bidId);
+    } catch (error) {
+        return res.status(400).json({message: "Invalid Object ID."});
+    }
+    bidId = ObjectId(bidId);
+
+    /* Only delete the bid if, after looping through user bids for the currently
     authenticated user, bid of matching id is found. Otherwise, return with an error.
      */ 
+    let found = false;
+    for (let i = 0; i < userBids.length; i++){
+        if ( bidId.equals(userBids[i]._id) ){
+            found = true;
+        }
+    }
+    // Return error if no matching bid is found.
+    if (!found){
+        return res.status(403).json({message: "Bid ID not found or not authorized for current user."});
+    }
 
-    let retval = await bidData.deleteBid(bidId);
-    // TODO - return to some page after request finishes.
+    //return OK status and JSON if delete successful, otherwise 500 and JSON
+    try {
+        const deletion = await bidData.deleteBid(bidId);
+        return res.status(200).json({message: "Bid successfully deleted."});
+    } catch (error) {
+        return res.status(500).json({message: "Bid deletion failed."});
+    }
 });
 
 module.exports =router;
